a471cec225e08ab44a8c717507ff22aad9440468
Entes-steinla <149874891+Entes-steinla@users.noreply.github.com>
2025-02-03 18:50:10 +0700

    fixed header main title

--- JS/sticky-scroll.js
+++ JS/sticky-scroll.js
@@ -1,10 +1,10 @@
 const stickyHeader = document.getElementById("sticky-header");
 const main = document.getElementById("main");
 
-window.addEventListener("scroll", () => {
-  if (window.scrollY > 300) {
-    stickyHeader.classList.add("hidden"); // Ẩn khi cuộn xuống quá 300px
+main.addEventListener("scroll", () => {
+  if (main.scrollTop > 300) {
+    stickyHeader.classList.add("hidden");
   } else {
-    stickyHeader.classList.remove("hidden"); // Hiện lại khi cuộn lên
+    stickyHeader.classList.remove("hidden");
   }
 });
