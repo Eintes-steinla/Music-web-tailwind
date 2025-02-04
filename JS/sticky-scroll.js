f412c3c1accc08aeff036ef855fe15949f42567d
Entes-steinla <149874891+Entes-steinla@users.noreply.github.com>
2025-02-04 22:35:47 +0700

    sticky scroll

--- JS/sticky-scroll.js
+++ JS/sticky-scroll.js
@@ -2,9 +2,11 @@ const stickyHeader = document.getElementById("sticky-header");
 const main = document.getElementById("main");
 
 main.addEventListener("scroll", () => {
-  if (main.scrollTop > 300) {
-    stickyHeader.classList.add("hidden");
-  } else {
+  if (main.scrollTop > 200) {
     stickyHeader.classList.remove("hidden");
+    stickyHeader.classList.remove("opacity-0");
+  } else {
+    stickyHeader.classList.add("hidden");
+    stickyHeader.classList.add("opacity-100");
   }
 });
