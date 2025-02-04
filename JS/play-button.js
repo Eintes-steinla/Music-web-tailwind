eec50bc3eb9a45a11e1814ec44a6e2e26d2b4e8a
Entes-steinla <149874891+Entes-steinla@users.noreply.github.com>
2025-02-04 21:40:17 +0700

    play slide carousel

--- JS/play-button.js
+++ JS/play-button.js
@@ -1,8 +1,19 @@
 document.querySelectorAll(".play-button-bg").forEach((button) => {
   button.addEventListener("click", () => {
-    const isChecked = document.querySelector(".play-button input").checked; // Lấy trạng thái của 1 nút
-    document.querySelectorAll(".play-button input").forEach((checkbox) => {
-      checkbox.checked = !isChecked; // Đồng bộ tất cả các nút
-    });
+    const songCheckbox = button.querySelector(".play-button-song input");
+
+    if (songCheckbox) {
+      const isChecked = songCheckbox.checked;
+      document
+        .querySelectorAll(".play-button-song input")
+        .forEach((checkbox) => {
+          checkbox.checked = !isChecked;
+        });
+    } else {
+      const checkbox = button.querySelector(".play-button input");
+      if (checkbox) {
+        checkbox.checked = !checkbox.checked;
+      }
+    }
   });
 });
