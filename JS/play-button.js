7c9b38daee5f8dedb242825e6dd55b87f6558f0e
Entes-steinla <149874891+Entes-steinla@users.noreply.github.com>
2025-02-06 17:32:38 +0700

    space play song update

--- JS/play-button.js
+++ JS/play-button.js
@@ -1,34 +1,42 @@
-document.querySelectorAll(".play-button-bg").forEach((button) => {
-  button.addEventListener("click", () => {
-    const songCheckbox = button.querySelector(".play-button-song input");
+function toggleCheckbox() {
+  document.querySelectorAll(".play-button-bg").forEach((button) => {
+    button.addEventListener("click", () => {
+      const songCheckbox = button.querySelector(".play-button-song input");
 
-    if (songCheckbox) {
-      const isChecked = songCheckbox.checked;
-      document
-        .querySelectorAll(".play-button-song input")
-        .forEach((checkbox) => {
-          checkbox.checked = !isChecked;
-        });
-    } else {
-      const checkbox = button.querySelector(".play-button input");
-      if (checkbox) {
-        checkbox.checked = !checkbox.checked;
+      if (songCheckbox) {
+        const isChecked = songCheckbox.checked;
+        document
+          .querySelectorAll(".play-button-song input")
+          .forEach((checkbox) => {
+            checkbox.checked = !isChecked;
+          });
+      } else {
+        const checkbox = button.querySelector(".play-button input");
+        if (checkbox) {
+          checkbox.checked = !checkbox.checked;
+        }
       }
-    }
+    });
   });
-});
+}
+
+// Gọi hàm khi cần thiết
+toggleCheckbox();
 
 // space play song
 document.addEventListener("keydown", function (event) {
   if (event.code === "Space") {
     event.preventDefault(); // Ngăn cuộn trang khi bấm Space
 
-    let audioSpace = document.getElementById("audio-song");
+    let audio = document.getElementById("audio-song");
+    let allLabels = document.querySelectorAll(".play-button-song input");
 
-    if (audioSpace.paused) {
-      audioSpace.play();
+    if (audio.paused) {
+      audio.play();
+      allLabels.forEach((input) => (input.checked = false));
     } else {
-      audioSpace.pause();
+      audio.pause();
+      allLabels.forEach((input) => (input.checked = true));
     }
   }
 });
